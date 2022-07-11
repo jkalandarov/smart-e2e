056f4d841e93f7f828f6d27184d628493be7fc13
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2022-07-11 17:59:59 +0500

    Intial setup

---
+++ playwright.config.ts
@@ -0,0 +1,41 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: 'html',
+ 
+  use: {
+    actionTimeout: 0,
+    baseURL: 'http://smartinvest.uz',
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+
+  outputDir: 'reports/',
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   port: 3000,
+  // },
+};
+
+export default config;
